2f69b04adec177d0ceed4436b91cdadc0351f2fc
Theorem411 <ziqil2@andrew.cmu.edu>
2024-04-04 17:04:57 -0400

    task 1: stream-processing init code

--- stream-processing/DataProducer/src/main/java/DataProducer.java
+++ stream-processing/DataProducer/src/main/java/DataProducer.java
@@ -1,8 +1,19 @@
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class DataProducer {
     private Producer<String, String> producer;
     private String traceFileName;
+
+    private Integer NumPartitions = 5;
 
     public DataProducer(Producer producer, String traceFileName) {
         this.producer = producer;
@@ -10,16 +21,38 @@ public class DataProducer {
     }
 
     /**
-      Task 1:
-        In Task 1, you need to read the content in the tracefile we give to you, 
-        create two streams, and feed the messages in the tracefile to different 
-        streams based on the value of "type" field in the JSON string.
-
-        Please note that you're working on an ec2 instance, but the streams should
-        be sent to your samza cluster. Make sure you can consume the topics on the
-        master node of your samza cluster before you make a submission.
-    */
+     * Task 1:
+     * In Task 1, you need to read the content in the tracefile we give to you,
+     * create two streams, and feed the messages in the tracefile to different
+     * streams based on the value of "type" field in the JSON string.
+     * 
+     * Please note that you're working on an ec2 instance, but the streams should
+     * be sent to your samza cluster. Make sure you can consume the topics on the
+     * master node of your samza cluster before you make a submission.
+     */
     public void sendData() {
+        try (BufferedReader br = new BufferedReader(new FileReader(traceFileName))) {
+            JsonParser parser = new JsonParser();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                JsonObject JSON = parser.parse(line).getAsJsonObject();
+                // Producer Record: topic, partition number, (opt) key, value
+                String Type = JSON.get("type").getAsString();
+                String Topic = (Type.equals("DRIVER_LOCATION"))
+                    ? "driver-locations"
+                    : "events";
+                
+                Integer BlockID = JSON.get("blockId").getAsInt();
+                producer.send(new ProducerRecord<String, String>(
+                    Topic, /* topic */
+                    BlockID % NumPartitions, /* partition id */
+                    null, /* key */
+                    line /* value */
+                ));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- stream-processing/DataProducer/src/main/java/DataProducerRunner.java
+++ stream-processing/DataProducer/src/main/java/DataProducerRunner.java
@@ -1,5 +1,9 @@
-public class DataProducerRunner {
+import java.util.Properties;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+
+public class DataProducerRunner {
     public static void main(String[] args) throws Exception {
         /*
             Tasks to complete:
@@ -9,6 +13,22 @@ public class DataProducerRunner {
             - Implement the sendData method as required in DataProducer
             - Call the sendData method to start sending data
         */
-            
+        // set up Kafka Producer
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        
+        // call DataProducer to send data stream to Kafka 
+        DataProducer dataProducer = new DataProducer(producer, "tracefile");
+        dataProducer.sendData();
+
     }
 }
