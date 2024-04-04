9ae04aac9d5db4e7a8c6f4b10cf30d5b913c7587
Theorem411 <ziqil2@andrew.cmu.edu>
2024-04-04 18:25:07 -0400

    fix DataProducerTest.java detailed test case

--- stream-processing/DataProducer/src/test/java/DataProducerTest.java
+++ stream-processing/DataProducer/src/test/java/DataProducerTest.java
@@ -34,8 +34,8 @@ public class DataProducerTest {
                 new ProducerRecord<>("events", 3, null, "{\"blockId\":5648,\"type\":\"ENTERING_BLOCK\"}"),
                 new ProducerRecord<>("driver-locations", 4, null, "{\"blockId\":5649,\"type\":\"DRIVER_LOCATION\"}"),
                 new ProducerRecord<>("events", 0, null, "{\"blockId\":5650,\"type\":\"RIDE_REQUEST\"}"),
-                new ProducerRecord<>("driver-locations", 1, null, "{\"blockId\":5651,\"type\":\"DRIVER_LOCATION\""),
-                new ProducerRecord<>("events", 2, null, "\"blockId\":5652,\"type\":LEAVING_BLOCK")
+                new ProducerRecord<>("driver-locations", 1, null, "{\"blockId\":5651,\"type\":\"DRIVER_LOCATION\"}"),
+                new ProducerRecord<>("events", 2, null, "\"blockId\":5652,\"type\":\"LEAVING_BLOCK\"}")
         );
 
         Assert.assertEquals("Producer records not matched!", expected, history);
